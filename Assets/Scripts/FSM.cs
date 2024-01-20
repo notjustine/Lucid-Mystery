a3ebe8731ad98be018d13680ce50722af0b0489b
RuYi-NG <90380321+RuYi-NG@users.noreply.github.com>
2024-01-20 12:54:01 -0500

    Update FSM.cs

--- Assets/Scripts/FSM.cs
+++ Assets/Scripts/FSM.cs
@@ -7,7 +7,7 @@ public enum StateType
 {
     Idle, Patrol, Chase, React, Attack, Search
 }
-//状态类别
+
 [Serializable]
 public class Parameter
 {
@@ -24,12 +24,12 @@ public class Parameter
     public float attackDamage;
     public Animator animator;
 }
+
 public class FSM : MonoBehaviour
 {
     public Parameter parameter;
 
     private IState currentState;
-
     private Dictionary<StateType, IState> states = new Dictionary<StateType, IState>();
 
     void Start()
@@ -45,13 +45,12 @@ public class FSM : MonoBehaviour
 
         parameter.animator = GetComponent<Animator>();
     }
-    //注册状态到状态机， 默认状态为Patrol， 获取动画控制器
 
-    // Update is called once per frame
     void Update()
     {
         currentState.OnUpdate();
     }
+
     public void TransitionState(StateType type)
     {
         if (currentState != null)
@@ -61,43 +60,20 @@ public class FSM : MonoBehaviour
         currentState = states[type];
         currentState.OnEnter();
     }
-    //切换状态
 
-    public void FlipTo(Transform target)
-    {
-        float Scalex = Mathf.Abs(gameObject.transform.localScale.x);
-        if (target != null)
-        {
-            if(transform.position.x > target.position.x)
-            {
-                transform.localScale = new Vector3(Scalex * -1, gameObject.transform.localScale.y * 1, gameObject.transform.localScale.z * 1);
-            }
-            else if(transform.position.x < target.position.x)
-            {
-                transform.localScale = new Vector3(Scalex * 1, gameObject.transform.localScale.y * 1, gameObject.transform.localScale.z * 1);
-            }
-        }
-    }
-    //如有目标，朝向目标
-    private void OnTriggerEnter2D(Collider2D other)
+    private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
-            {
+        {
             parameter.target = other.transform;
-            }
+        }
     }
-    //视线中如有玩家，玩家变为目标
 
-    private void OnTriggerExit2D(Collider2D other)
+    private void OnTriggerExit(Collider other)
     {
         if (other.CompareTag("Player"))
         {
             parameter.target = null;
         }
-    }
-    //视线中丢失玩家，失去目标
-    private void OnDrawGizmos()
-    {
-        Gizmos.DrawWireSphere(parameter.attackPoint.position, parameter.attackArea);
     }
 }
