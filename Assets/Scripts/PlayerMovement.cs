74b7a853e0ef31dcaf8187a5f8f83044620af744
jess-zhai <jessica.zhai@mail.utoronto.ca>
2024-01-20 14:17:28 -0500

    edit

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public CharacterController controller;
-    public float directionSize = 0.3f;
+    public float directionSize = 10f;
     public Transform cam;
 
     public KeyCode moveForward = KeyCode.UpArrow;
@@ -14,32 +14,39 @@ public class PlayerMovement : MonoBehaviour
     public KeyCode moveRight = KeyCode.RightArrow;
     public KeyCode attack = KeyCode.Mouse0;
 
+    private float movementCooldown = 0.3f;
+    private float lastMoveTime;
+
     // Update is called once per frame
     void Update()
     {
-        Vector3 direction = Vector3.zero;
+        if (Time.time - lastMoveTime >= movementCooldown)
+        {
+            Vector3 direction = Vector3.zero;
 
-        if (Input.GetKey(moveForward))
-        {
-            direction += Vector3.forward;
-        }
-        if (Input.GetKey(moveBackward))
-        {
-            direction += Vector3.back;
-        }
-        if (Input.GetKey(moveLeft))
-        {
-            direction += Vector3.left;
-        }
-        if (Input.GetKey(moveRight))
-        {
-            direction += Vector3.right;
-        }
+            if (Input.GetKey(moveForward))
+            {
+                direction += Vector3.forward;
+            }
+            if (Input.GetKey(moveBackward))
+            {
+                direction += Vector3.back;
+            }
+            if (Input.GetKey(moveLeft))
+            {
+                direction += Vector3.left;
+            }
+            if (Input.GetKey(moveRight))
+            {
+                direction += Vector3.right;
+            }
 
-        if (direction != Vector3.zero)
-        {
-            direction = Quaternion.Euler(0f, cam.eulerAngles.y, 0f) * direction;
-            controller.Move(direction * directionSize);
+            if (direction != Vector3.zero)
+            {
+                direction = Quaternion.Euler(0f, cam.eulerAngles.y, 0f) * direction;
+                controller.Move(direction * directionSize);
+                lastMoveTime = Time.time; // Update the last movement time
+            }
         }
         if (Input.GetKey(attack))
         {
