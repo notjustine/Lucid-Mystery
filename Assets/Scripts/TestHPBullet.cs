6d1761968cb946e16463b717284f73ba2687af6a
jess-zhai <jessica.zhai@mail.utoronto.ca>
2024-02-14 20:55:16 -0500

    modified TestHPBulet script and cube location

--- Assets/Scripts/TestHPBullet.cs
+++ Assets/Scripts/TestHPBullet.cs
@@ -2,11 +2,25 @@ using UnityEngine;
 public class TestHPBullet : MonoBehaviour
 {
     public float damage = 20f;
+    public float speed = 20f;
 
     private void Start()
     {
+        Vector3 direction = (Vector3.zero - transform.position).normalized;
+        GetComponent<Rigidbody>().velocity = direction * speed;
         Destroy(gameObject, 10f);
     }
+
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        // Check if the collided object is not the player
+        if (collision.gameObject.tag != "Player" | collision.gameObject.tag != "Enviorment")
+        {
+            Destroy(gameObject); // Destroy bullet on collision
+        }
+    }
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
