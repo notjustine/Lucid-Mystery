a67028074ab2c8a02c10edb2677d2eac36b19243
dylanhollohan <dylanhollohan@gmail.com>
2024-02-17 15:36:53 -0500

    ignore collisions between boss bullet and boss

--- Assets/Scripts/BulletController.cs
+++ Assets/Scripts/BulletController.cs
@@ -9,17 +9,22 @@ detection, and deletion after time of bullets that missed.
 */
 public class BulletController : MonoBehaviour
 {
-    const float sniperBulletSpeed = 80f;
+    const float sniperBulletSpeed = 60f;
     const float rotationSpeed = 100f;
     const float maxLifetime = 1f;
     float bulletLifetime;
     private Vector3 rot;
     GameObject sniper;
+    GameObject boss;
+    private CapsuleCollider bulletCollider;
+    private CapsuleCollider bossCollider;
 
 
     void Start()
     {
         sniper = GameObject.Find("Sniper");
+        boss = GameObject.Find("Cube");  // We should fix this name.
+
         bulletLifetime = 0f;
         // Below: calculates a vector that is one "frame" closer to sniper.transform.forward direction 
         rot = Vector3.RotateTowards(transform.forward, sniper.transform.forward, rotationSpeed, 0.0f); // last one is the error margin of rotation
@@ -28,6 +33,11 @@ public class BulletController : MonoBehaviour
 
         Rigidbody bulletBody = GetComponent<Rigidbody>();
         bulletBody.AddForce(transform.forward * sniperBulletSpeed, ForceMode.VelocityChange);
+
+        //  ignore collisions between bullet and boss:
+        bulletCollider = GetComponent<CapsuleCollider>();
+        bossCollider = boss.GetComponent<CapsuleCollider>();
+        Physics.IgnoreCollision(bulletCollider, bossCollider);
     }
 
 
