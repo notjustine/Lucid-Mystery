b7e21966dfb4715e908394190c45a319cf13b11c
dylanhollohan <dylanhollohan@gmail.com>
2024-02-25 18:42:30 -0500

    docs

--- Assets/Scripts/SprayAttackController.cs
+++ Assets/Scripts/SprayAttackController.cs
@@ -5,46 +5,38 @@ using UnityEngine;
 
 public class SprayAttackController : MonoBehaviour
 {
-    // Start is called before the first frame update
     GameObject turretsRig;
     float timeSinceFired;
     const float turretRotationSpeed = 20f;
-    // float turretRotationStep;
     ShootSprayBullet[] turrets;
     bool hasNotFired;
 
+
     void Start()
     {
-        // Grab the 'turrets' empty object
         turretsRig = GameObject.Find("turrets");
         hasNotFired = true;
         timeSinceFired = 0f;
         turrets = FindObjectsOfType<ShootSprayBullet>();
     }
 
-    // Update is called once per frame
+
     void Update()
     {
-        // Test the rotation of the turretsRig
-        // StartCoroutine(RotateToTargetCoroutine());
 
-
-        // Trigger the firing of turrets
+        // Trigger the firing of turrets on a timer for now.
         timeSinceFired += Time.deltaTime;
-        if (hasNotFired || timeSinceFired > 12)
+        if (hasNotFired || timeSinceFired > 6)
         {
-            // foreach (ShootSprayBullet turret in turrets)
-            // {
             StartCoroutine(TripleShootAndRotate(turrets));
             hasNotFired = false;
             timeSinceFired = 0;
-            // }
         }
     }
 
     IEnumerator TripleShootAndRotate(ShootSprayBullet[] turrets)
     {
-        // After rotation is fixed, use a coroutine that waits for the 15 degree rotation to complete before firing again.
+        // First round of shots.
         foreach (ShootSprayBullet turret in turrets)
         {
             turret.Shoot();
@@ -53,21 +45,21 @@ public class SprayAttackController : MonoBehaviour
         Quaternion currentRotation = turretsRig.transform.rotation;
         Quaternion rotationToAdd = Quaternion.Euler(0f, -15f, 0f);
         Quaternion targetRotation = currentRotation * rotationToAdd;
-        // Debug.Log($"old: {turretsRig.transform.rotation.y}");
-        // targetRotation.eulerAngles = new Vector3(turretsRig.transform.rotation.x, turretsRig.transform.rotation.y + 15f, turretsRig.transform.rotation.z);
-        // Debug.Log($"new: {targetRotation.y}");
+
         while (turretsRig.transform.rotation != targetRotation)
         {
             turretsRig.transform.rotation = Quaternion.RotateTowards(turretsRig.transform.rotation, targetRotation, turretRotationSpeed * Time.deltaTime);
             yield return null;
         }
-        yield return new WaitForSeconds(0.5f);
 
+        yield return new WaitForSeconds(0.5f);  // adds some padding between shoot and rotations
+
+        // Second round of shots.
         foreach (ShootSprayBullet turret in turrets)
         {
             turret.Shoot();
         }
-        // CODE WORKS UNTIL HERE.
+
         currentRotation = turretsRig.transform.rotation;
         targetRotation = currentRotation * rotationToAdd;
         while (turretsRig.transform.rotation != targetRotation)
@@ -75,7 +67,10 @@ public class SprayAttackController : MonoBehaviour
             turretsRig.transform.rotation = Quaternion.RotateTowards(turretsRig.transform.rotation, targetRotation, turretRotationSpeed * Time.deltaTime);
             yield return null;
         }
-        yield return new WaitForSeconds(0.5f);
+
+        yield return new WaitForSeconds(0.5f); // adds some padding between shoot and rotations
+
+        // Third and final round of shots until attack is triggered again.
         foreach (ShootSprayBullet turret in turrets)
         {
             turret.Shoot();
