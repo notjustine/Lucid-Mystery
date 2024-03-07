28b7bfd6820b26f15f3faeeac43f455223c4ad2f
justine m <justinemgbt@gmail.com>
2024-03-07 01:06:57 -0500

    updated health bar sprites
    - split boss health bar into two

--- Assets/Scripts/BossScripts/BossHealth.cs
+++ Assets/Scripts/BossScripts/BossHealth.cs
@@ -4,24 +4,27 @@ using UnityEngine.UI;
 
 public class BossHealth : MonoBehaviour
 {
-    [SerializeField] public float maxHealth = 2000f;
+    [SerializeField] public float maxHealth = 1000f;
     public float currHealth;
     public bool isInvulnerable = false;
     [SerializeField] private int phase = 0;
 
-    public HealthBar healthBar;
+    public HealthBar healthBar;     // phase 1
+    public HealthBar healthBar2;    // phase 2
 
     // Start is called before the first frame update
     public void Start()
     {
         currHealth = maxHealth;
-        healthBar.SetSliderMax(maxHealth);
+        healthBar.SetSliderMax(maxHealth / 2);
+        healthBar2.SetSliderMax(maxHealth / 2);
         phase = PlayerPrefs.GetInt("bossPhase");
     }
 
     public void TakeDamage(float amount)
     {
         currHealth -= amount;
+
         if (currHealth <= 0)
         {
             currHealth = 0;
@@ -30,9 +33,13 @@ public class BossHealth : MonoBehaviour
         else if (currHealth <= maxHealth / 2)
         {
             PhaseTwo();
+            healthBar.SetSlider(0f);
+            healthBar2.SetSlider(currHealth);
         }
-
-        healthBar.SetSlider(currHealth);
+        else
+        {
+            healthBar.SetSlider(currHealth - (maxHealth / 2));
+        }
     }
 
     private void Update()
