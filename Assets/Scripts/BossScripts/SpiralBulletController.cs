24ca0a4071a5913fcd7cfe3af94f0d4be19bdddf
dylanhollohan <dylanhollohan@gmail.com>
2024-03-14 12:59:27 -0400

    sprayBulletController renamed SpiralBulletController

--- Assets/Scripts/BossScripts/SpiralBulletController.cs
+++ Assets/Scripts/BossScripts/SpiralBulletController.cs
@@ -7,7 +7,7 @@ using UnityEngine;
 Dylan:  this handles the flight direction of our sniper bullets from the boss, collision
 detection, and deletion after time of bullets that missed.
 */
-public class SprayBulletController : MonoBehaviour
+public class SpiralBulletController : MonoBehaviour
 {
     const float maxLifetime = 2f;
     float bulletLifetime;
@@ -16,22 +16,22 @@ public class SprayBulletController : MonoBehaviour
     private CapsuleCollider bulletCollider;
     private CapsuleCollider bossCollider;
     private DifficultyManager difficultyManager;
-    private float sprayBulletDamage;
-    private float sprayBulletSpeed;
+    private float spiralBulletDamage;
+    private float spiralBulletSpeed;
 
     void Start()
     {
         // Set up difficulty settings
         difficultyManager = DifficultyManager.Instance;
-        sprayBulletDamage = difficultyManager.GetValue(DifficultyManager.StatName.SPRAY_BULLET_DAMAGE);
-        sprayBulletSpeed = difficultyManager.GetValue(DifficultyManager.StatName.SPRAY_BULLET_SPEED);
-        
+        spiralBulletDamage = difficultyManager.GetValue(DifficultyManager.StatName.SPIRAL_BULLET_DAMAGE);
+        spiralBulletSpeed = difficultyManager.GetValue(DifficultyManager.StatName.SPIRAL_BULLET_SPEED);
+
         boss = GameObject.Find("Cube");  // We should fix this name.
 
         bulletLifetime = 0f;
 
         Rigidbody bulletBody = GetComponent<Rigidbody>();
-        bulletBody.AddForce(transform.forward * sprayBulletSpeed, ForceMode.VelocityChange);
+        bulletBody.AddForce(transform.forward * spiralBulletSpeed, ForceMode.VelocityChange);
 
         //  ignore collisions between bullet and boss:
         bulletCollider = GetComponent<CapsuleCollider>();
@@ -65,7 +65,7 @@ public class SprayBulletController : MonoBehaviour
             PlayerStatus playerStatus = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerStatus>(); ;
             if (playerStatus != null) // Check if the PlayerStatus component is found
             {
-                playerStatus.TakeDamage(sprayBulletDamage);
+                playerStatus.TakeDamage(spiralBulletDamage);
             }
             else
             {
