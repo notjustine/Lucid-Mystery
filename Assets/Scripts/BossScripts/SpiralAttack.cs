35a609736001564e70a6f4e5debe013ab62d2bbe
dylanhollohan <dylanhollohan@gmail.com>
2024-03-15 14:12:26 -0400

    Minor spiral refactor, preps WarningManager usage

--- Assets/Scripts/BossScripts/SpiralAttack.cs
+++ Assets/Scripts/BossScripts/SpiralAttack.cs
@@ -10,9 +10,11 @@ public class SpiralAttack : MonoBehaviour
     GameObject turretsRig;
     GameObject targetsParent;
     const float turretRotationSpeed = 20f;
+    const float warningTime = 0.5f;
     ShootSpiralBullet[] turrets;
     int currTargetIndex;
     const int NUM_SLICES = 24;
+    private WarningManager warningManager;
 
     void Start()
     {
@@ -20,6 +22,8 @@ public class SpiralAttack : MonoBehaviour
         targetsParent = GameObject.Find("SpiralTargets");
         currTargetIndex = 0;
         turrets = FindObjectsOfType<ShootSpiralBullet>();
+        // Flash warning on turrets themselves when attacking
+        warningManager = WarningManager.Instance;
     }
 
 
@@ -35,11 +39,7 @@ public class SpiralAttack : MonoBehaviour
     IEnumerator TripleShootAndRotate(ShootSpiralBullet[] turrets)
     {
         // First round of shots.
-        foreach (ShootSpiralBullet turret in turrets)
-        {
-            turret.Shoot();  // assume shoots at first index of 0 
-        }
-        turrets[0].PlaySound();
+        Shoot(turrets);
         // increment the target index because we already shot at above.
         currTargetIndex = (currTargetIndex + 1) % NUM_SLICES;
         // Determine position of child to shoot at.
@@ -55,11 +55,7 @@ public class SpiralAttack : MonoBehaviour
 
         yield return new WaitForSeconds(0.5f);  // adds some padding between shoot and rotations
         // Second round of shots.
-        foreach (ShootSpiralBullet turret in turrets)
-        {
-            turret.Shoot();
-        }
-        turrets[0].PlaySound();
+        Shoot(turrets);
         // increment the target index
         currTargetIndex = (currTargetIndex + 1) % NUM_SLICES;
         // Determine position of child to shoot at
@@ -76,10 +72,29 @@ public class SpiralAttack : MonoBehaviour
         yield return new WaitForSeconds(0.5f);
 
         // Third and final round of shots until attack is triggered again.
+        Shoot(turrets);
+    }
+
+    private void Shoot(ShootSpiralBullet[] turrets)
+    {
         foreach (ShootSpiralBullet turret in turrets)
         {
             turret.Shoot();
         }
         turrets[0].PlaySound();
+    }
+
+    public List<string> GetWarningObjects()
+    {
+        return new List<string> {
+            "turret1",
+            "turret2",
+            "turret3",
+            "turret4",
+            "turret5",
+            "turret6",
+            "turret7",
+            "turret8",
+        };
     }
 }
