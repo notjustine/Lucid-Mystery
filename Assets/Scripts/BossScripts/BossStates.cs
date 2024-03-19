bfcb48011cdfa9fe6469857a8f0f90f6a84e1f88
Jordan <jordan>
2024-03-19 07:54:20 -0400

    removes coroutine stopping on sleep

--- Assets/Scripts/BossScripts/BossStates.cs
+++ Assets/Scripts/BossScripts/BossStates.cs
@@ -53,7 +53,7 @@ public class BossStates : MonoBehaviour
     {
         if (isSleeping)
         {
-            StopAllCoroutines();            
+            // StopAllCoroutines();            
             return;
         }
 
