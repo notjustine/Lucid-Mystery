6a0f58bd7ba011c7af0766af2ca7c16176f284ed
dylanhollohan <dylanhollohan@gmail.com>
2024-03-26 19:25:03 -0400

    display warning on full spiral column

--- Assets/Scripts/BossScripts/SpiralAttack.cs
+++ Assets/Scripts/BossScripts/SpiralAttack.cs
@@ -15,6 +15,7 @@ public class SpiralAttack : MonoBehaviour
     int currTargetIndex;
     const int NUM_SLICES = 24;
     private WarningManager warningManager;
+    private Dictionary<int, List<string>> turretGroups;
 
     void Start()
     {
@@ -22,8 +23,9 @@ public class SpiralAttack : MonoBehaviour
         targetsParent = GameObject.Find("SpiralTargets");
         currTargetIndex = 0;
         turrets = FindObjectsOfType<ShootSpiralBullet>();
-        // Flash warning on turrets themselves when attacking
         warningManager = WarningManager.Instance;
+        turretGroups = new Dictionary<int, List<string>>();
+        InitTurretGroups();
     }
 
 
@@ -39,10 +41,10 @@ public class SpiralAttack : MonoBehaviour
     IEnumerator TripleShootAndRotate(ShootSpiralBullet[] turrets)
     {
         // Offer warning blinks
-        warningManager.ToggleWarning(GetWarningObjects(), true, WarningManager.WarningType.SPIRAL);
+        List<string> warned = warningManager.ToggleWarning(GetWarningObjects(), true, WarningManager.WarningType.SPIRAL);
         yield return new WaitForSeconds(warningTime);  // adds some padding between shoot and rotations
         // First round of shots.
-        warningManager.ToggleWarning(GetWarningObjects(), false, WarningManager.WarningType.SPIRAL);
+        warningManager.ToggleWarning(warned, false, WarningManager.WarningType.SPIRAL);
         Shoot(turrets);
         // increment the target index because we already shot at above.
         currTargetIndex = (currTargetIndex + 1) % NUM_SLICES;
@@ -56,9 +58,9 @@ public class SpiralAttack : MonoBehaviour
             turretsRig.transform.rotation = Quaternion.RotateTowards(turretsRig.transform.rotation, firstTargetRotation, turretRotationSpeed * Time.deltaTime);
             yield return null;
         }
-        warningManager.ToggleWarning(GetWarningObjects(), true, WarningManager.WarningType.SPIRAL);
+        warned = warningManager.ToggleWarning(GetWarningObjects(), true, WarningManager.WarningType.SPIRAL);
         yield return new WaitForSeconds(warningTime);  // adds some padding between shoot and rotations
-        warningManager.ToggleWarning(GetWarningObjects(), false, WarningManager.WarningType.SPIRAL);
+        warningManager.ToggleWarning(warned, false, WarningManager.WarningType.SPIRAL);
         // Second round of shots.
         Shoot(turrets);
         // increment the target index
@@ -73,9 +75,9 @@ public class SpiralAttack : MonoBehaviour
             turretsRig.transform.rotation = Quaternion.RotateTowards(turretsRig.transform.rotation, secondTargetRotation, turretRotationSpeed * Time.deltaTime);
             yield return null;
         }
-        warningManager.ToggleWarning(GetWarningObjects(), true, WarningManager.WarningType.SPIRAL);
+        warned = warningManager.ToggleWarning(GetWarningObjects(), true, WarningManager.WarningType.SPIRAL);
         yield return new WaitForSeconds(warningTime);
-        warningManager.ToggleWarning(GetWarningObjects(), false, WarningManager.WarningType.SPIRAL);
+        warningManager.ToggleWarning(warned, false, WarningManager.WarningType.SPIRAL);
 
         // Third and final round of shots until attack is triggered again.
         Shoot(turrets);
@@ -92,7 +94,8 @@ public class SpiralAttack : MonoBehaviour
 
     public List<string> GetWarningObjects()
     {
-        return new List<string> {
+        List<string> warnings = new List<string>
+        {
             "turret1",
             "turret2",
             "turret3",
@@ -101,6 +104,50 @@ public class SpiralAttack : MonoBehaviour
             "turret6",
             "turret7",
             "turret8",
+        };
+        warnings.AddRange(turretGroups[currTargetIndex % 3]);
+        Debug.Log(warnings);
+        return warnings;
+    }
+
+
+    private void InitTurretGroups()
+    {
+        turretGroups[0] = new List<string>
+        {
+            $"R1_18", $"R2_18", $"R3_18", $"R4_18",
+            $"R1_15", $"R2_15", $"R3_15", $"R4_15",
+            $"R1_12", $"R2_12", $"R3_12", $"R4_12",
+            $"R1_09", $"R2_09", $"R3_09", $"R4_09",
+            $"R1_06", $"R2_06", $"R3_06", $"R4_06",
+            $"R1_03", $"R2_03", $"R3_03", $"R4_03",
+            $"R1_24", $"R2_24", $"R3_24", $"R4_24",
+            $"R1_21", $"R2_21", $"R3_21", $"R4_21",
+        };
+        Debug.Log(turretGroups[0]);
+
+        turretGroups[1] = new List<string>
+        {
+            $"R1_17", $"R2_17", $"R3_17", $"R4_17",
+            $"R1_14", $"R2_14", $"R3_14", $"R4_14",
+            $"R1_11", $"R2_11", $"R3_11", $"R4_11",
+            $"R1_08", $"R2_08", $"R3_08", $"R4_08",
+            $"R1_05", $"R2_05", $"R3_05", $"R4_05",
+            $"R1_02", $"R2_02", $"R3_02", $"R4_02",
+            $"R1_23", $"R2_23", $"R3_23", $"R4_23",
+            $"R1_20", $"R2_20", $"R3_20", $"R4_20",
+        };
+
+        turretGroups[2] = new List<string>
+        {
+            $"R1_16", $"R2_16", $"R3_16", $"R4_16",
+            $"R1_13", $"R2_13", $"R3_13", $"R4_13",
+            $"R1_10", $"R2_10", $"R3_10", $"R4_10",
+            $"R1_07", $"R2_07", $"R3_07", $"R4_07",
+            $"R1_04", $"R2_04", $"R3_04", $"R4_04",
+            $"R1_01", $"R2_01", $"R3_01", $"R4_01",
+            $"R1_22", $"R2_22", $"R3_22", $"R4_22",
+            $"R1_19", $"R2_19", $"R3_19", $"R4_19",
         };
     }
 }
